36c6b8fe617b70e5f159f4480b35c920ba951f3c
Florian HENRY <florian.henry@open-concept.pro>
2021-10-05 17:30:00 +0200

    better test if douscount supplier is set

--- erpnext_france/erpnext_france/custom_scripts_py/purchase_invoice.py
+++ erpnext_france/erpnext_france/custom_scripts_py/purchase_invoice.py
@@ -10,33 +10,36 @@ from erpnext.accounts.general_ledger import flt, make_entry
 
 def correct_gl_entry_supplier_discount(doc, method):
     if doc is not None and doc.discount_amount != 0:
+        discount_account = frappe.get_cached_value('Company', doc.company, "discount_supplier_account")
+        if discount_account is not None:
 
-        # Build Item accountancy code dict to know what line update into GL Entry
-        item_code_and_net = {}
-        for docline in doc.items:
-            item_code_and_net[docline.expense_account] = docline.amount
+            # Build Item accountancy code dict to know what line update into GL Entry
+            item_code_and_net = {}
+            for docline in doc.items:
+                item_code_and_net[docline.expense_account] = docline.amount
 
-        gl_invoice_entries = frappe.get_list("GL Entry", filters={'voucher_no': doc.name, 'voucher_type': doc.doctype})
-        if gl_invoice_entries is not None and len(gl_invoice_entries) != 0:
+            gl_invoice_entries = frappe.get_list("GL Entry",
+                                                 filters={'voucher_no': doc.name, 'voucher_type': doc.doctype})
+            if gl_invoice_entries is not None and len(gl_invoice_entries) != 0:
 
-            precision = frappe.get_precision("GL Entry", "debit_in_account_currency")
+                precision = frappe.get_precision("GL Entry", "debit_in_account_currency")
 
-            for gl_invoice_entry in gl_invoice_entries:
-                gl_entry = frappe.get_doc('GL Entry', gl_invoice_entry.name)
-                if gl_entry.party_type == "Supplier":
-                    gl_entry.db_set('credit', doc.net_total)
-                    gl_entry.db_set('credit_in_account_currency', doc.net_total)
-                elif gl_entry.account in item_code_and_net.keys():
-                    gl_entry.db_set('debit', item_code_and_net[gl_entry.account])
-                    gl_entry.db_set('debit_in_account_currency', item_code_and_net[gl_entry.account])
+                for gl_invoice_entry in gl_invoice_entries:
+                    gl_entry = frappe.get_doc('GL Entry', gl_invoice_entry.name)
+                    if gl_entry.party_type == "Supplier":
+                        gl_entry.db_set('credit', doc.net_total)
+                        gl_entry.db_set('credit_in_account_currency', doc.net_total)
+                    elif gl_entry.account in item_code_and_net.keys():
+                        gl_entry.db_set('debit', item_code_and_net[gl_entry.account])
+                        gl_entry.db_set('debit_in_account_currency', item_code_and_net[gl_entry.account])
 
-        new_gl_entry = doc.get_gl_dict({
-            "account": doc.get_company_default("discount_supplier_account"),
-            "against": doc.supplier_name,
-            "cost_center": doc.get_company_default("cost_center"),
-            "project": doc.get("project"),
-            "remarks": "Supplier Discount",
-            "credit": flt(doc.discount_amount, precision),
-            "is_opening": "No"
-        })
-        make_entry(new_gl_entry, adv_adj=False, update_outstanding='No')
+            new_gl_entry = doc.get_gl_dict({
+                "account": discount_account,
+                "against": doc.supplier_name,
+                "cost_center": doc.get_company_default("cost_center"),
+                "project": doc.get("project"),
+                "remarks": "Supplier Discount",
+                "credit": flt(doc.discount_amount, precision),
+                "is_opening": "No"
+            })
+            make_entry(new_gl_entry, adv_adj=False, update_outstanding='No')
